e7b261f47836bc9aa741eec012a94c0681b96c65
Гёля Лис <48104777+prvln@users.noreply.github.com>
2020-12-17 18:26:37 +0300

    Update main.c

--- Laba2/main.c
+++ Laba2/main.c
@@ -5,7 +5,7 @@
 #include <time.h> 
 int main(){
     int pipa[2];
-    char format[80] = "I'm [%i], a beloved child of [%i], today is %s";
+    char format[80] = "I'm [%i], a beloved parent of [%i], today is %s";
     time_t seconds;
     if (pipe(pipa) == -1) return 1;
 
@@ -14,21 +14,22 @@ int main(){
     switch(childsPID)
         {
             case 0:{
-                char sent[80] = "";
-                seconds = time(NULL);
-                sprintf(sent, format, getpid(), getppid() ,asctime(localtime(&seconds)));
-                write(pipa[1], sent, 80);
-                break;
-            }
-            default:{
                 sleep (6);
                 char receive[80] = "";
                 seconds = time(NULL);
                 read(pipa[0], &receive, 80);
 
-                printf("Hi, i'm [%i], read a message from my child [%i]:\n\n\"\n%s\"\n\n", getpid(), childsPID, receive);
+                printf("Hi, i'm [%i]. Why dont you read a message from my parent [%i]:\n\n\"\n%s\"\n\n", getpid(), getppid(), receive);
                 printf("He is 6 seconds late! Now it's %s", asctime(localtime(&seconds)));
-                break;
+                break; 
+            }
+            default:{
+                char sent[80] = "";
+                seconds = time(NULL);
+                sprintf(sent, format, getpid(), childsPID ,asctime(localtime(&seconds)));
+                write(pipa[1], sent, 80);
+                waitpid(childsPID,0,0);
+                break;                
             }
         }
     return 0;
